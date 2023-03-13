d7dc5209b234bb756ba84c5406d7a4eac712dcb8
g-pg <gabrielpgusso@gmail.com>
2023-03-13 19:41:45 -0300

    cleanup

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,25 +1,22 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    speed: 1000,
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-    autoplay: {
-        enabled: true,
-        delay: 3000,
-    },
-  });
+const swiper = new Swiper(".swiper", {
+	direction: "horizontal",
+	loop: true,
+	speed: 1000,
+
+	pagination: {
+		el: ".swiper-pagination",
+	},
+
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+
+	scrollbar: {
+		el: ".swiper-scrollbar",
+	},
+	autoplay: {
+		enabled: true,
+		delay: 3000,
+	},
+});
